2edc0235c3d12127500119dac2f18c7e2bf8d4d9
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-07-28 18:13:39 +0530

    add rejister controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { rejisterUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 
 const router = Router()
 
-router.route("/rejister").post(rejisterUser)
+router.route("/rejister").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    rejisterUser
+)
 
 export default router
